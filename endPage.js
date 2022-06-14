561df3601a1a533966dd16315e44eaaabd88f230
CChendy <xychencindy@gmail.com>
2022-06-14 07:41:43 -0700

    Update endPage.js

--- endPage.js
+++ endPage.js
@@ -1,3 +1,16 @@
 window.onbeforeunload = function() {
     return "Please, don't go back or refresh the page.";
 }
+
+deleteAllCookies();
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
